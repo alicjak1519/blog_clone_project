8286c319ba0222ded64f429ac3751bafac42e59a
Alicja <akrzeminska1519@gmail.com>
2020-05-09 21:46:31 +0200

    create: CRUD methods added

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from blog import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    # path('')
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('about', views.AboutView),
+    path('post/<str:pk>', views.PostDetailView, name='post_detail'),
+    path('post/create', views.PostCreateView, name='post_create'),
+    path('post/<str:pk>/edit', views.PostUpdatePost, name='post_edit'),
+    path('post/<str:pk>/delete', views.PostDeletePost, name='post_delete'),
+    path('drafts', views.DraftListView, name='post_draft_list'),
 ]
